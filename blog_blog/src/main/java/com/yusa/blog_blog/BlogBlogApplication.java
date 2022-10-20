b30f604ce8746f463b862da3859aabb94e942edd
nnu1tiFragnnent <1628585992@qq.com>
2022-10-20 19:28:57 +0800

    项目结构

---
+++ blog_blog/src/main/java/com/yusa/blog_blog/BlogBlogApplication.java
@@ -0,0 +1,13 @@
+package com.yusa.blog_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogBlogApplication.class, args);
+    }
+
+}
---
+++ blog_framework/src/main/java/com/yusa/blog_framework/BlogFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.yusa.blog_framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogFrameworkApplication.class, args);
+    }
+
+}

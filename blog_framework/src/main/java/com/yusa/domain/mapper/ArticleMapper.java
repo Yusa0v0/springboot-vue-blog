1f086d561a60c64440831b1bcda8bdbb864a442d
nnu1tiFragnnent <1628585992@qq.com>
2022-10-20 22:58:04 +0800

    Article

---
+++ blog_framework/src/main/java/com/yusa/domain/mapper/ArticleMapper.java
@@ -0,0 +1,12 @@
+package com.yusa.domain.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yusa.domain.entity.Article;
+
+/**
+ * @author Yusa
+ * @version 1.0
+ */
+public interface ArticleMapper extends BaseMapper<Article> {
+
+}
---
+++ blog_framework/src/main/java/com/yusa/domain/service/ArticleService.java
@@ -0,0 +1,11 @@
+package com.yusa.domain.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.yusa.domain.entity.Article;
+
+/**
+ * @author Yusa
+ * @version 1.0
+ */
+public interface ArticleService extends IService<Article> {
+}

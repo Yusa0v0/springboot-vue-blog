3202c1fb039584c098082056c21786c86acba545
nnu1tiFragnnent <1628585992@qq.com>
2022-10-20 17:04:03 +0800

    项目结构

---
+++ blog_blog/src/test/java/com/yusa/blog_blog/BlogBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yusa.blog_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
